2ac023c1414da88faa52d02b6c492f4f5bee259d
Dave <davidbassey23@gmail.com>
2023-08-10 10:33:28 -0700

    a program that prints sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints sizes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n" (unsigned long)Size of (a));
+	printf("Size of an int: %lu byte(s)\n" (unsigned long)Size of (b));
+	printf("Size of a long int: %lu byte(s)\n" (unsigned long)Size of (c));
+	printf("size of a long long int: %lu byte(s)\n" (unsigned long)Size of (d));
+	printf("Size of a float: %lu byte(s)\n" (unsigned long)Size of (e));
+	return (0);
+}
