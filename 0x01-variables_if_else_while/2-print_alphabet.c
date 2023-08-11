e151900a6f8398bb43919b7fd9dee8b18739f94c
Dave <davidbassey23@gmail.com>
2023-08-11 08:38:15 -0700

    a program that prints alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always (Success0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z', c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
