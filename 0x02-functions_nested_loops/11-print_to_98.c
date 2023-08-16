b48804cce4ca0ec855562885dcaa5735117402e9
Dave <davidbassey23@gmail.com>
2023-08-15 18:14:52 -0700

    corrections to function and nested loops file 10 and 12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 - prints all natural numbers from n to 98,
+ * follow by a new line
  * @n: print from this number
  */
 
@@ -19,7 +20,8 @@ void print_to_98(int n)
 			else if (i == 98)
 				printf("%d\n", i);
 		}
-	} else if (n >= 98)
+	}
+	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j++)
 		{
