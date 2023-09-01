e564a3b3cf03362378ab30187cb35936187bbf06
Dave <davidbassey23@gmail.com>
2023-09-01 15:31:56 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -33,11 +33,12 @@ int check_num(char *str)
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char argv[])
+
+int main(int argc, char *argv[])
 {
 	/*Declaring variables*/
 	int count;
-	int src_to_int;
+	int str_to_int;
 	int sum = 0;
 
 	count = 1;
