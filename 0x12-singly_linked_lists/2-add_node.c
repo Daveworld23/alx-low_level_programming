b9a68d2ae9cda800a9d719b69acf405075dafca8
daveworld23 <davidbassey23@gmail..com>
2023-09-21 11:27:29 -0700

    function that adds a new mode to the begining of a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "linked_list.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - function that adds a new node at the beginning of a list
+ * @head: double pointer to the list
+ * @str: new string to add to the node
+ * Return: address of new element, or Null if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_n;
+	unsigned int len;
+
+	for (len = 0; str[len]; len++)
+		;
+	new_n = malloc(sizeof(list_t));
+	if (!new_n)
+		return (NULL);
+	new_n->str = strdup(str);
+	new_n->len = len;
+	new_n->next = (*head);
+	(*head) = new_n;
+
+	return (*head);
+}
