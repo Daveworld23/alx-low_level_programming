05dbcc015c6fd6e41abcf71936ada760999a239d
daveworld23 <davidbassey23@gmail..com>
2023-09-21 10:41:00 -0700

    test file that writes a function that list the number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "linked_list.h"
+#include <stdlib.h>
+
+/**
+ * list_len - function that list the number of element
+ * @h: pointer to the list_t list
+ *
+ * Return: Number of elments
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
