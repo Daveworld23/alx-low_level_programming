d07bf5dcd0212dfbf1d8b68d7679b41347bfa5ff
daveworld23 <davidbassey23@gmail..com>
2023-09-21 12:20:42 -0700

    free the singly linked list created by deallovating the memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "linked_list.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list
+ * @head: the linked list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *last_n;
+
+	while (head)
+	{
+		last_n = head->next;
+		free(head->str);
+		free(head);
+		head = last_n;
+	}
+}
