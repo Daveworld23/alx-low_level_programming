68a1a09762f08454bc8aeabfc9a5822e747ecedf
daveworld23 <davidbassey23@gmail..com>
2023-09-25 14:21:44 -0700

    function that frees a linked list with a pointer to pointer of the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a list
+ * @head: the linked list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *last_n;
+
+	while (*head)
+	{
+		last_n = (*head)->next;
+		free(*head);
+		*head = last_n;
+	}
+	*head = NULL;
+}
