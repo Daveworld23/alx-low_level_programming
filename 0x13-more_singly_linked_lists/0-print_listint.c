348d461a3fc9d6f78f6a15ad89bc15a08994534c
daveworld23 <davidbassey23@gmail..com>
2023-09-25 21:47:19 -0700

    correction to task one of more singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-			printf("Error\n");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		nodes++;
 		h = h->next;
 	}
