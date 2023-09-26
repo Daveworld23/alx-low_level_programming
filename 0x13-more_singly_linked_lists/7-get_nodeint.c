5ab17e70ae92a98c1e78c1dbe432f63b952e420e
daveworld23 <davidbassey23@gmail..com>
2023-09-26 04:20:08 -0700

    debugging task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *last_n = head;
 	unsigned int i = 0;
 
-	if (!head || !index)
+	if (!head)
 		return (NULL);
 	while (last_n && i < index)
 	{
