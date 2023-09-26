9a87175701d6c5e7184cb1ca122a8d98efd0c6bc
daveworld23 <davidbassey23@gmail..com>
2023-09-26 04:27:35 -0700

    debugging task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,6 +10,8 @@ int sum_listint(listint_t *head)
 	listint_t *last_n = head;
 	int sum = 0;
 
+	if (head == NULL)
+		return (0);
 	for (; last_n != NULL; last_n = last_n->next)
 	{
 		sum += last_n->n;
