de059dde5d0c0198793eebcf3e6d9fc00ee25512
daveworld23 <davidbassey23@gmail..com>
2023-09-26 07:40:12 -0700

    debugging task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *last_n = *head;
 	unsigned int i;
 
-	if (!head || !idx)
+	if (!head)
 		return (NULL);
 	new_n = malloc(sizeof(listint_t));
 	if (!new_n)
