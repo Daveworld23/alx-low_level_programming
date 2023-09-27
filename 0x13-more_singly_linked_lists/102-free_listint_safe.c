7ad9408467d107c598a6be25372deede2dd3e542
daveworld23 <davidbassey23@gmail..com>
2023-09-26 20:41:00 -0700

    function that frees a linked list with a single loop

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to node
+ * Description: function frees a list with a loop that goes through
+ * the list once and sets the head to NULL
+ * Return: size of freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	listint_t *last_n;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		if (*h > (*h)->next)
+		{
+			last_n = (*h)->next;
+			free(*h);
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
