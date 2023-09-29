f3447f1a79e8b3af0766aea9bc0d9e481357f4a2
daveworld23 <davidbassey23@gmail..com>
2023-09-29 01:51:52 -0700

    convertunv binary to unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,14 +14,19 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		n <<= 1;
-		if (b[i] == '1')
-			n += 1;
+		if (b[i] == '0')
+		{
+			n <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			n <<= 1;
+			n |= 1;
+		}
+		else
+		{
+			return  (0);
+		}
 	}
 	return (n);
 }
