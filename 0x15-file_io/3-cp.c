44339bbac70e40a3fc062d522ef50c055f9e6a7c
daveworld23 <davidbassey23@gmail..com>
2023-10-03 07:28:20 -0700

    function that copies the content of one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,53 +2,70 @@
 
 #define BUFF_SIZE 1024
 
+void close_f(int file_d);
+
+/**
+ * close_f - close file descriptor
+ * @file_d: file descriptor
+ */
+void close_f(int file_d)
+{
+	int i;
+
+	i = close(file_d);
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_d);
+		exit(100);
+	}
+}
+
 /**
  * main - Copies from one file to the other
  * @argc: Number of argument passed to the program
  * @argv: Array of pointers to arguments
- *
  * Return: 0 on success, otherwise exist.
  */
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
-	char *buff[BUFF_SIZE];
+	char buff[BUFF_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	*buff = (argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buff, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	for (; r > 0;)
+	if (from == -1)
 	{
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buff);
-			exit(98);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
+		exit(99);
+		close(from);
+	}
+	while ((r = read(from, buff, BUFF_SIZE)) > 0)
+	{
 		w = write(to, buff, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			close(from);
+			close(to);
 			exit(99);
 		}
-		r = read(from, buff, 1024);
-		to = open(argv[2], O_WRONGLY | O_APPEND);
 	}
-	free(buff);
-	close(from);
-	close(to);
-	if (close == -1)
+	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n" close);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(from);
+		close(to);
+		exit(98);
 	}
 	return (0);
 }
-
