0eefb24e659611044568525cf898400934e1062d
Daveworld23 <davidbassey23@gmail.com>
2023-11-23 09:03:15 -0800

    function that clear bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - function that sets a bit at index 0
+ * @n: pointer to the value to be set
+ * @index: index to set value
+ * Return: 1 for success, -1 for error
+ */
+int clear_bit(unaigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
