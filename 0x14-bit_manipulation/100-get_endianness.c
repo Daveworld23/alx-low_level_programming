46491bfcc88305c082475f39485589441bb5a000
Daveworld23 <davidbassey23@gmail.com>
2023-11-23 09:58:49 -0800

    more task on bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - function that checks for endianness
+ *
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *b = (char *)&a;
+
+	if (*b == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - flips from one bit to another
+ * @n: first number to flip
+ * @m: second number to flip to
+ * Return: number of flipped bits
+ */
+unsigned int flip_bits(unsigned long int *n, unsigned long int m)
+{
+	int count = 0;
+	unsigned long int diff = n ^ m;
+
+	while (diff)
+	{
+		diff &= (diff - 1);
+		count++;
+	}
+	return (count);
+}
