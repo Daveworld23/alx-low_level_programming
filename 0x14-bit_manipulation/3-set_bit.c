ea8fd5db5551d1b8a6780c3bd6c87d91a9f7adb3
Daveworld23 <davidbassey23@gmail.com>
2023-11-23 08:58:07 -0800

    function that sets bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,20 @@
-#include <stdio.h>
+#include "main.c"
+
+/**
+ * set_bit - function that sets bit
+ * @n: pointer to number to be set
+ * @index: index of the set number
+ * Return: 1 if success, -1 for error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1));
+	{
+		return (-1);
+	}
+	set = 1 << index;
+	*n = *n | set;
+	return (1);
+}
