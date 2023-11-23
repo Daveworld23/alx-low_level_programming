a9fb0c5678cc503aaa9d25f000345a5caae80ad0
Daveworld23 <davidbassey23@gmail.com>
2023-11-23 08:30:04 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer to string of binary number
+ * Return: converted string or 0 for error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int i;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			n <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			n <<= 1;
+			n |= 1;
+		}
+		else
+		{
+			return (0)(
+		}
+	}
+	return (n);
+}
