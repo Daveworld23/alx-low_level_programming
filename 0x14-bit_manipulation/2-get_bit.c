b49c7a500a10d6171653e6a815f9776abc16d5fb
Daveworld23 <davidbassey23@gmail.com>
2023-11-23 08:51:59 -0800

    function that gets bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,24 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - function that gets a binary number
+ * @n: value to check for bit
+ * @index: insex to check for bit
+ * Return: value of bit at undex, otherwise -1 if empty
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int base, check;
+
+	for (; index > (sizeof(unsigned long int) * 8 - 1); index++)
+	{
+		return (-1);
+	}
+	base = 1 << index;
+	check = n & base;
+	if (check == base)
+	{
+		return (1);
+	}
+	return (0);
+}
