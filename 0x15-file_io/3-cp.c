e61b945c62009bfe6a2391f36389ebdb9fe39802
Daveworld23 <davidbassey23@gmail.com>
2023-11-27 07:15:31 -0800

    debugging task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ void close_fd(int file_des)
 	i = close(file_des);
 	if (i == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_des);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_des);
 		exit(100);
 	}
 }
@@ -33,19 +33,19 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usuage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usuage: cp file_from file_to\n");
 		exit(97);
 	}
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((r = read(from, buff, BUFF_SIZE)) > 0)
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 		w = write(to, buff, r);
 		if (w == -1)
 		{
-			dprintf(2, "Erro: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close_fd(from);
 			close_fd(to);
 			exit(99);
