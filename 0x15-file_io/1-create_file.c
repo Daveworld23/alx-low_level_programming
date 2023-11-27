a892260ed981ea273fcdd22af72164fc2df109ed
Daveworld23 <davidbassey23@gmail.com>
2023-11-27 06:55:50 -0800

    debugging task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (file_des == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	if (text_content != NULL)
 	{
 		for (w = 0; text_content[w]; w++)
